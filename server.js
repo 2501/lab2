e0ec80e9d9be76778d805d374c661961067aaf41
Timothy Clark <timeeeee@gmail.com>
2014-10-02 11:55:18 -0500

    Change campus to dictionary with room id as key. Update app.delete(/:id/:item).

--- server.js
+++ server.js
@@ -53,16 +53,34 @@ app.get('/images/:name', function(req, res){
 
 app.delete('/:id/:item', function(req, res){
         var userid = req.cookies.userid;
+        user = users.userid;
+        room = campus.id;
+        res.set({'Content-Type': 'application/json'});
+        res.status(200);
+        if (room.what == undefined) room.what = [];
+        if (id = user.local && item in room.what) {
+	        user.inventory.push(item);
+	        res.send(userid.inventory);
+	        delete room.what[item];
+	        return;
+        }
+        res.send([]);
+        return;
+}
+        
+
 	for (var i in campus) {
 		if (req.params.id == campus[i].id) {
 		    res.set({'Content-Type': 'application/json'});
+		    // ix will represent the index of the item
 		    var ix = -1;
 		    if (campus[i].what != undefined) {
 					ix = campus[i].what.indexOf(req.params.item);
 		    }
-		    if (ix >= 0) {
+		    // If room exists, and user is in the room, and item is there, take item
+		    if (ix >= 0 && users["local"] == campus[ix]["id"]) {
 		       res.status(200);
-			inventories[userid].push(campus[i].what[ix]); // stash
+			user["inventory"].push(campus[i].what[ix]); // stash
 		        res.send(inventories[userid]);
 			campus[i].what.splice(ix, 1); // room no longer has this
 			return;
@@ -111,72 +129,69 @@ var dropbox = function(userid, ix,room) {
 	room.what.push(item);
 }
 
-var inventories = {};
-var locations = {};
-
 var campus =
-    [ { "id": "lied-center",
+    { "lied-center" : {
 	"where": "LiedCenter.jpg",
 	"next": {"east": "eaton-hall", "south": "dole-institute"},
 	"text": "You are outside the Lied Center.",
 	"who": []
+    },
+      "dole-institute" : {
+	  "where": "DoleInstituteofPolitics.jpg",
+	  "next": {"east": "allen-fieldhouse", "north": "lied-center"},
+	  "text": "You take in the view of the Dole Institute of Politics. This is the best part of your walk to Nichols Hall.",
+	  "who": []
       },
-      { "id": "dole-institute",
-	"where": "DoleInstituteofPolitics.jpg",
-	"next": {"east": "allen-fieldhouse", "north": "lied-center"},
-	"text": "You take in the view of the Dole Institute of Politics. This is the best part of your walk to Nichols Hall.",
-	"who": []
+      "eaton-hall" : {
+	  "where": "EatonHall.jpg",
+	  "next": {"east": "snow-hall", "south": "allen-fieldhouse", "west": "lied-center"},
+	  "text": "You are outside Eaton Hall. You should recognize here.",
+	  "who": []
       },
-      { "id": "eaton-hall",
-	"where": "EatonHall.jpg",
-	"next": {"east": "snow-hall", "south": "allen-fieldhouse", "west": "lied-center"},
-	"text": "You are outside Eaton Hall. You should recognize here.",
-	"who": []
+      "snow-hall" : {
+	  "where": "SnowHall.jpg",
+	  "next": {"east": "strong-hall", "south": "ambler-recreation", "west": "eaton-hall"},
+	  "text": "You are outside Snow Hall. Math class? Waiting for the bus?",
+	  "who": []
       },
-      { "id": "snow-hall",
-	"where": "SnowHall.jpg",
-	"next": {"east": "strong-hall", "south": "ambler-recreation", "west": "eaton-hall"},
-	"text": "You are outside Snow Hall. Math class? Waiting for the bus?",
-	"who": []
+      "strong-hall" : {
+	  "where": "StrongHall.jpg",
+	  "next": {"east": "outside-fraser", "north": "memorial-stadium", "west": "snow-hall"},
+	  "what": ["coffee"],
+	  "text": "You are outside Strong Hall.",
+	  "who": []
       },
-      { "id": "strong-hall",
-	"where": "StrongHall.jpg",
-	"next": {"east": "outside-fraser", "north": "memorial-stadium", "west": "snow-hall"},
-	"what": ["coffee"],
-	"text": "You are outside Strong Hall.",
-	"who": []
+      "ambler-recreation" : {
+	  "where": "AmblerRecreation.jpg",
+	  "next": {"west": "allen-fieldhouse", "north": "snow-hall"},
+	  "text": "It's the starting of the semester, and you feel motivated to be at the Gym. Let's see about that in 3 weeks.",
+	  "who": []
       },
-      { "id": "ambler-recreation",
-	"where": "AmblerRecreation.jpg",
-	"next": {"west": "allen-fieldhouse", "north": "snow-hall"},
-	"text": "It's the starting of the semester, and you feel motivated to be at the Gym. Let's see about that in 3 weeks.",
-	"who": []
+      "outside-fraser" : {
+	  "where": "OutsideFraserHall.jpg",
+	  "next": {"west": "strong-hall","north":"spencer-museum"},
+	  "what": ["basketball"],
+	  "text": "On your walk to the Kansas Union, you wish you had class outside.",
+	  "who": []
       },
-      { "id": "outside-fraser",
-  "where": "OutsideFraserHall.jpg",
-	"next": {"west": "strong-hall","north":"spencer-museum"},
-	"what": ["basketball"],
-	"text": "On your walk to the Kansas Union, you wish you had class outside.",
-	"who": []
+      "spencer-museum" : {
+	  "where": "SpencerMuseum.jpg",
+	  "next": {"south": "outside-fraser","west":"memorial-stadium"},
+	  "what": ["art"],
+	  "text": "You are at the Spencer Museum of Art.",
+	  "who": []
       },
-      { "id": "spencer-museum",
-	"where": "SpencerMuseum.jpg",
-	"next": {"south": "outside-fraser","west":"memorial-stadium"},
-	"what": ["art"],
-	"text": "You are at the Spencer Museum of Art.",
-	"who": []
+      "memorial-stadium" : {
+	  "where": "MemorialStadium.jpg",
+	  "next": {"south": "strong-hall","east":"spencer-museum"},
+	  "what": ["ku flag"],
+	  "text": "Half the crowd is wearing KU Basketball gear at the football game.",
+	  "who": []
       },
-      { "id": "memorial-stadium",
-	"where": "MemorialStadium.jpg",
-	"next": {"south": "strong-hall","east":"spencer-museum"},
-	"what": ["ku flag"],
-	"text": "Half the crowd is wearing KU Basketball gear at the football game.",
-	"who": []
-      },
-      { "id": "allen-fieldhouse",
-	"where": "AllenFieldhouse.jpg",
-	"next": {"north": "eaton-hall","east": "ambler-recreation","west": "dole-institute"},
-	"text": "Rock Chalk! You're at the field house.",
-	"who": []
+      "allen-fieldhouse" : {
+	  "where": "AllenFieldhouse.jpg",
+	  "next": {"north": "eaton-hall","east": "ambler-recreation","west": "dole-institute"},
+	  "text": "Rock Chalk! You're at the field house.",
+	  "who": []
       }
     ]
